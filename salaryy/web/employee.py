82bfb65982c9d1d9673d711e8140bc4bb4354b54
bvpav <bvpav@tutanota.com>
2021-02-14 18:30:54 +0200

    Added employee edit and delete

--- salaryy/web/employee.py
+++ salaryy/web/employee.py
@@ -32,6 +32,11 @@ def add():
 
         if not employee_name:
             error = 'Name is required'
+        elif db.execute(
+            'SELECT * from employee WHERE name = ? AND employer_id = ?',
+            (employee_name, g.user['id'])
+        ).fetchone() is not None:
+            error = 'Name must be unique'
         elif employee_salary:
             try:
                 employee_salary = float(employee_salary)
@@ -60,4 +65,54 @@ def add():
 def detail(employee_name):
     employee = get_employee(employee_name)
     return render_template('employee/detail.html', employee=employee)
+
+@bp.route('/edit/<string:employee_name>', methods=('GET', 'POST'))
+@login_required
+def edit(employee_name):
+    employee = get_employee(employee_name)
+
+    if request.method == 'POST':
+        # XXX: Repeating code fron add()
+        employee_name = request.form['employee_name']
+        employee_salary = request.form['employee_salary']
+        db = get_db()
+        error = None
+
+        if not employee_name:
+            error = 'Name is required'
+        elif db.execute(
+            'SELECT * from employee WHERE id != ? AND name = ? AND employer_id = ?',
+            (employee['id'], employee_name, g.user['id'])
+        ).fetchone() is not None:
+            error = 'Name must be unique'
+        elif employee_salary:
+            try:
+                employee_salary = float(employee_salary)
+            except ValueError:
+                error = 'Salary must be a number'
+            else:
+                if employee_salary < 0.0:
+                    error = 'Salary can\'t be negative'
+
+        if error is None:
+            db.execute(
+                'UPDATE employee SET name = ?, salary = ? WHERE id = ?',
+                (employee_name, employee_salary, employee['id'])
+            )
+            db.commit()
+            flash(f'Employee {employee_name} successfully updated')
+            return redirect(url_for('budget.dashboard'))
+
+        flash(error)
+
+    return render_template('employee/add.html', employee=employee)
+
+
+@bp.route('/delete/<string:employee_name>', methods=('POST',))
+def delete(employee_name):
+    employee = get_employee(employee_name)
+    db = get_db()
+    db.execute('DELETE FROM employee WHERE id = ?', (employee['id'],))
+    db.commit()
+    return redirect(url_for('budget.dashboard'))
 
