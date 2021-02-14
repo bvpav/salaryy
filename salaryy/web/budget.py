0259f2812d26e9bbd56fa80d8595c49958c8a0ce
bvpav <bvpav@tutanota.com>
2021-02-14 15:30:55 +0200

    Implemented employee view in the dashboard

--- salaryy/web/budget.py
+++ salaryy/web/budget.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, g
 
 from .auth import login_required
+from .db import get_db
 
 bp = Blueprint('budget', __name__)
 
@@ -9,5 +10,11 @@ bp = Blueprint('budget', __name__)
 @bp.route('/dashboard')
 @login_required
 def dashboard():
-    return render_template('budget/dashboard.html')
+    db = get_db()
+    employees = db.execute(
+        'SELECT * FROM employee WHERE employer_id = ?',
+        (g.user['id'],)
+    ).fetchall()
+    print(employees[0]['name'])
+    return render_template('budget/dashboard.html', employees=employees)
 
