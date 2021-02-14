bbf7b14de06b511428a3f0e880cba96dfc025e1c
bvpav <bvpav@tutanota.com>
2021-02-14 16:15:09 +0200

    Added employee detail view

--- salaryy/web/budget.py
+++ salaryy/web/budget.py
@@ -12,7 +12,7 @@ bp = Blueprint('budget', __name__)
 def dashboard():
     db = get_db()
     employees = db.execute(
-        'SELECT * FROM employee WHERE employer_id = ?',
+        'SELECT * FROM employee WHERE employer_id = ? ORDER BY name',
         (g.user['id'],)
     ).fetchall()
     return render_template('budget/dashboard.html', employees=employees)
--- salaryy/web/employee.py
+++ salaryy/web/employee.py
@@ -1,9 +1,24 @@
+import functools
+
 from flask import Blueprint, render_template, request, g, flash, redirect, url_for
+from werkzeug.exceptions import abort
 
 from .auth import login_required
 from .db import get_db
 
 bp = Blueprint('employee', __name__, url_prefix='/employee')
+
+
+def get_employee(employee_name):
+    employee = get_db().execute(
+        'SELECT * FROM employee WHERE employer_id = ? AND name=?',
+        (g.user['id'], employee_name)
+    ).fetchone()
+
+    if employee is None:
+        abort(404, f'Employee {employee_name} not found.')
+
+    return employee
 
 
 @bp.route('/add', methods=('GET', 'POST'))
@@ -43,5 +58,6 @@ def add():
 @bp.route('/detail/<string:employee_name>', methods=('GET', 'POST'))
 @login_required
 def detail(employee_name):
-    return render_template('employee/detail.html', employee_name=employee_name)
+    employee = get_employee(employee_name)
+    return render_template('employee/detail.html', employee=employee)
 
